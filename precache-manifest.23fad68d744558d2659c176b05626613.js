a7d0e99f2a1f75bf90530ccef77de11f9c989550
Rohannagrut <rgnagrut_b20@ee.vjti.ac.in>
2023-01-27 23:43:37 +0530

    Updates

---
+++ precache-manifest.23fad68d744558d2659c176b05626613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9e31244d418bd74ebedc6570a210f4",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "7fc02ad5ba666471c21d",
+    "url": "/To-do-list/static/css/main.5b8bd228.chunk.css"
+  },
+  {
+    "revision": "b0284d23a7d9ae3b0a88",
+    "url": "/To-do-list/static/js/2.7460f878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.7460f878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc02ad5ba666471c21d",
+    "url": "/To-do-list/static/js/main.4476ea30.chunk.js"
+  },
+  {
+    "revision": "64b0509380757713198e",
+    "url": "/To-do-list/static/js/runtime-main.757c6418.js"
+  }
+]);
